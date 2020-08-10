bf58839bdd95c8063ba3d489e20f31bfb3960e6a
Livanenkova <arina909@yandex.ru>
2020-08-10 21:44:17 +0300

    setting up a webpack, dividing into modules

---
+++ src/view/film-details-popup.js
@@ -0,0 +1,10 @@
+export const createFilmDetailsPopup = () => {
+  return (
+    `<section class="film-details">
+      <form class="film-details__inner" action="" method="get">
+        <div class="form-details__top-container"></div>
+        <div class="form-details__bottom-container"></div>
+      </form>
+    </section>`
+  );
+};
---
+++ src/view/films-list-extra-title.js
@@ -0,0 +1,5 @@
+export const createFilmsListExtraTitle = (title) => {
+  return (
+    `<h2 class="films-list__title">${title}</h2>`
+  );
+};
